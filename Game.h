c0392bd6a875cc4a611936e3441a4ef3dbd4cb12
RobertLita <lita.robert@yahoo.com>
2022-12-19 23:47:21 +0200

    added difficulty levels: walls & reversed controls

--- Game.h
+++ Game.h
@@ -1,3 +1,4 @@
+#include "Arduino.h"
 #ifndef Game
 #define Game
 
@@ -5,17 +6,28 @@
 #include "StorageManager.h"
 #include "Matrix.h"
 
+struct params {
+  byte deltaSpeed;
+  byte scorePerPoint;
+  bool hasWalls;
+  bool reversedControls; 
+};
 
 struct position {
   byte x, y;
 };
 
 position snake[MATRIX_SIZE * MATRIX_SIZE];
+position walls[9];
+const params config[] = { { 8, 1, false, false },
+                          { 11, 2, true, false },
+                          { 9, 3, true, true },
+                        };
 
 unsigned long long lastMoved, foodBlinkTimestamp;
 
 bool sJoyMoved = false, foodDot;
-byte snakeX, snakeY ;
+short snakeX, snakeY ;
 
 byte foodPosX, foodPosY;
 
@@ -23,10 +35,15 @@ short snakeXDirection, snakeYDirection, snakeLength, moveInterval;;
 
 void updateMatrix(byte foodX, byte foodY);
 void generateFood();
+void generateWalls();
 
 void gameSetup() {
+  randomSeed(analogRead(0));
   clearMatrixLeds();
-  moveInterval = 500;
+
+  if (config[settings.difficulty - 1].hasWalls)
+    generateWalls();
+  moveInterval = 450;
   snakeX = 0;
   snakeY = 2;  
   snakeLength = 3;
@@ -50,18 +67,53 @@ void gameSetup() {
 void generateFood() {
   bool invalidPosition = true;
   while (invalidPosition) {
-    foodPosX = rand() % 8;
-    foodPosY = rand() % 8;
+    invalidPosition = false;
+    foodPosX = random(8);
+    foodPosY = random(8);
     for (byte i = 0; i < snakeLength; i++) 
       if (snake[i].x == foodPosX && snake[i].y == foodPosY) {
         invalidPosition = true;
         break;
       }
-    else invalidPosition = false;
+    if (!invalidPosition && config[settings.difficulty - 1].hasWalls)
+      for (byte i = 0; i < 9; i++)
+        if (walls[i].x == foodPosX && walls[i].y == foodPosY) {
+          invalidPosition = true;
+          break;
+        }
   }
 
 }
 
+void generateWalls() {
+  byte previousRow = 10, previousColumn = 10, startingPointX, startingPointY;
+  bool generatedWrong;
+  for (byte i = 0; i < 3; i++) {
+    byte wallDirection = random(2);
+    generatedWrong = true;
+    while(generatedWrong) {
+      startingPointX = random(1, 5);
+      startingPointY = random(1, 5);
+      Serial.print(startingPointX);
+      Serial.print(startingPointY);
+      Serial.print('\n');
+      if (abs(int(startingPointX - previousRow)) > 1 && abs(int(startingPointY - previousColumn)) > 1) 
+        generatedWrong = false;
+    }
+    
+    for (byte j = 0; j < 3; j++)
+      if (wallDirection == 0) {
+        matrix[startingPointX][startingPointY + j] = 1;
+        walls[i * 3 + j] = {startingPointX, startingPointY + j};
+      }
+      else {
+        matrix[startingPointX + j][startingPointY] = 1;
+        walls[i * 3 + j] = {startingPointX + j, startingPointY};
+      }
+    previousRow = startingPointX;
+    previousColumn = startingPointY;
+  }
+}
 
 void blinkFood () {
   unsigned long timePassed = millis() - foodBlinkTimestamp;
@@ -83,6 +135,10 @@ bool endCondition() {
   for (byte i = 1; i < snakeLength; i++) 
     if (snake[i].x == snakeX && snake[i].y == snakeY)
       return true;
+  if (config[settings.difficulty - 1].hasWalls)
+    for (byte i = 0; i < 9; i++) 
+      if (walls[i].x == snakeX && walls[i].y == snakeY)
+        return true;
   return false;
 }
 
@@ -90,28 +146,36 @@ bool endCondition() {
 void getSnakeDirection() {
   int xValue = analogRead(pinY);
   int yValue = analogRead(pinX);
-
+  bool reversed = config[settings.difficulty - 1].reversedControls;
   if (stillMinThreshold <= xValue && xValue <= stillMaxThreshold && stillMinThreshold <= yValue && yValue <= stillMaxThreshold) {
     sJoyMoved = false;
   }
 
   if (xValue > maxThreshold && sJoyMoved == false && snakeXDirection == STILL) {
-    snakeXDirection = RIGHT;
+    if (!reversed)
+      snakeXDirection = RIGHT;
+    else snakeXDirection = LEFT;
     snakeYDirection = STILL;
     sJoyMoved = true;
   } else if (xValue < minThreshold && sJoyMoved == false && snakeXDirection == STILL) {
-    snakeXDirection = LEFT;
+    if (!reversed)
+      snakeXDirection = LEFT;
+    else snakeXDirection = RIGHT;
     snakeYDirection = STILL;
     sJoyMoved = true;
   }
 
   if (yValue > maxThreshold && sJoyMoved == false && snakeYDirection == STILL) {
     snakeXDirection = STILL;
-    snakeYDirection = UP;
+    if (!reversed)
+      snakeYDirection = UP;
+    else snakeYDirection = DOWN;
     sJoyMoved = true;
   } else if (yValue < minThreshold && sJoyMoved == false && snakeYDirection == STILL) {
     snakeXDirection = STILL;
-    snakeYDirection = DOWN;
+    if (!reversed)
+      snakeYDirection = DOWN;
+    else snakeYDirection = UP;
     sJoyMoved = true;
   }
 }
@@ -120,6 +184,9 @@ void moveSnake() {
 
   snakeX += snakeXDirection;
   snakeY += snakeYDirection;
+  Serial.print(snake[snakeLength - 1].x);
+  Serial.print(snake[snakeLength - 1].y);
+  Serial.print('\n');
 
   matrix[snake[snakeLength - 1].x][snake[snakeLength - 1].y] = 0;
   for (byte i = snakeLength - 1; i >= 1; i--)
@@ -159,7 +226,7 @@ void gameLoop(bool &updateLCD) {
     matrixChanged = true;
     generateFood();
     snakeLength++;
-    moveInterval -= 8;
+    moveInterval -= config[settings.difficulty - 1].deltaSpeed;
     updateLCD = true;
   }
 
@@ -171,7 +238,7 @@ void gameLoop(bool &updateLCD) {
 }
 
 short getPoints() {
-  return snakeLength - 3;
+  return config[settings.difficulty - 1].scorePerPoint * (snakeLength - 3);
 }
 
 #endif Game
